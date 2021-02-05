0b6a3acfdfdd91375c382009bf8661c2af225410
ifeslopes <ifes.lopes@gmail.com>
2021-02-05 20:51:07 -0300

    Handling exceptions and clearing screen

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,12 @@
 package aplication;
 
 import boardgame.Board;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,16 +15,28 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         while (true) {
-            UI.printBord(chessMatch.getPieces());
-            System.out.println();
-            System.out.println("Source: ");
-            ChessPosition source = UI.readChessposition(sc);
-            System.out.println();
-            System.out.println("Target");
-            ChessPosition tartget = UI.readChessposition(sc);
+         try {
 
-            ChessPiece capturedPiece = chessMatch.performaChessMove(source,tartget);
 
+
+             UI.printBord(chessMatch.getPieces());
+             System.out.println();
+             System.out.println("Source: ");
+             ChessPosition source = UI.readChessposition(sc);
+             System.out.println();
+             System.out.println("Target");
+             ChessPosition tartget = UI.readChessposition(sc);
+
+             ChessPiece capturedPiece = chessMatch.performaChessMove(source, tartget);
+         }
+         catch (ChessException e){
+             System.out.println(e.getMessage());
+             sc.nextLine();
+         }
+         catch (InputMismatchException e){
+             System.out.println(e.getMessage());
+             sc.nextLine();
+         }
 
 
         }
--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -28,6 +28,8 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+
+
     public static ChessPosition readChessposition(Scanner sc){
        try {
            String s = sc.nextLine();
